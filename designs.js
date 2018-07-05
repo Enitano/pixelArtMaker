a4bfe6e602ae944f73b4fe80a2daa08fac952521
Enitano <40745262+Enitano@users.noreply.github.com>
2018-07-05 08:47:16 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+ $("#sizePicker").submit(function makeGrid(grid){
+
+  var row = $("#inputHeight").val();
+
+  var column = $("#inputWeight").val();
+
+   for (var i = 1; i <= row; i++){
+
+
+    $("table").append("<tr></tr>");
+
+    for (var e = 1; e <= column; e++){
+
+
+     //append colum to last row
+
+     $("tr:last").append("<td class='cells'></td>");
+ 
+    }
+
+   }
+
+   grid.preventDefault();
+
+
+   $("#pixelCanvas").click(function(event){
+    var cellColor = $("#colorPicker").val();
+    $(event.target).css("background-color", cellColor);
+   });
+ });
+
+});
